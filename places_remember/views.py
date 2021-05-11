fd7b80c24f2a0ee8cbac3a549567403371c753ee
nizepart <55020360+nizepart@users.noreply.github.com>
2021-05-11 17:45:55 +0300

    Show only author's posts

--- places_remember/views.py
+++ places_remember/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 @login_required(login_url='login')
 def index(request):
     memories = Memory.objects.filter(author=request.user).order_by('-id')
